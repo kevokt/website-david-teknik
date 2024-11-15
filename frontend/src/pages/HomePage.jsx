2b9b2c3c6f7bf67c29c763c5fb2e7b6ebb4dc9f0
kevokt <kevinoktavian08@gmail.com>
2024-11-15 14:10:02 +0700

    small progress on HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,22 +1,29 @@
 import React, { useEffect } from "react";
-import { Container, Text, VStack, SimpleGrid } from "@chakra-ui/react";
+import { Container, Text, VStack, SimpleGrid, Box } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { useProductStore } from "../store/product";
 import ProductCard from "../components/ProductCard";
 
 const HomePage = () => {
   return (
-    <Container maxW={"Container.x1"} py={12}>
-      <VStack spacing={8}>
-        <Text
-          fontSize={"30"}
-          fontWeight={"bold"}
-          bgGradient={"linear(to-r, cyan.400, blue.500)"}
-          bgClip={"text"}
-          textAlign={"center"}
+    <Container maxW={"Container.x1"} py={5}>
+      <VStack spacing={3}>
+        <Box
+          bgSize="cover"
+          bgImage="url(https://images.unsplash.com/photo-1579618215542-2ed5e10b65ed?q=80&w=1937&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)"
+          w="100vw"
+          h={"xl"}
+          pt={10}
         >
-          Home
-        </Text>
+          <Text
+            fontSize={"30"}
+            fontWeight={"bold"}
+            color={"white"}
+            textAlign={"center"}
+          >
+            Selamat Datang di <br /> CV. David Teknik Blower
+          </Text>
+        </Box>
       </VStack>
     </Container>
   );
